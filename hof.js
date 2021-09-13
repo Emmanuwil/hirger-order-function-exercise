17a999031101d04b08d8f75f7204405c996b2427
Emmanuwil <simonemmanuwil@gmail.com>
2021-09-12 21:53:55 -0400

    hof exercise

---
+++ hof.js
@@ -0,0 +1,12 @@
+console.log("Hello World!\n==========\n");
+
+//Exercise 1 Section
+console.log("Exercise 1:\n==========\n");
+
+ function plus(number) {
+     return(plusNumber) => (plusNumber += number)
+ }
+
+ plus15 = plus(15);
+
+ console.log(plus15(10));
